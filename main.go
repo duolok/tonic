d1196a7c98477d3003edd76407edbb2f5831e4f2
duolok <dusan.lecic5@gmail.com>
2024-08-23 18:45:15 +0200

    add: basic server setup

--- main.go
+++ main.go
@@ -1,56 +1,24 @@
 package main
 
 import (
-    "fmt"
-    "github.com/jdxyw/generativeart"
-    "github.com/jdxyw/generativeart/arts"
-    "github.com/jdxyw/generativeart/common"
-    "image/color"
-    "math/rand"
-    "time"
+    "github.com/gin-gonic/gin"
+    "tonic/generator"
 )
 
-var DRAWINGS = map[string]generativeart.Engine{
-    "maze":      arts.NewMaze(10),
-    "julia":     arts.NewJulia(func(z complex128) complex128 { return z*z + complex(-0.1, 0.651) }, 40, 1.5, 1.5),
-    "randcicle": arts.NewRandCicle(30, 80, 0.2, 2, 10, 30, true),
-    "blackhole": arts.NewBlackHole(200, 400, 0.01),
-    "janus":     arts.NewJanus(5, 10),
-    "random":    arts.NewRandomShape(150),
-    "silksky":   arts.NewSilkSky(15, 5),
-    "circles":   arts.NewColorCircle2(30),
+func router() *gin.Engine {
+    r := gin.Default()
+    userRoute := r.Group("/image")
+    {
+        userRoute.GET("/circles", func(c *gin.Context) {
+            file := generator.DrawOne("circles")
+            c.Header("Content-Type", "image/png")
+            c.File(file)
+        })
+    }
+
+    return r
 }
 
 func main() {
-    drawMany(DRAWINGS)
+    router().Run()
 }
-
-func drawMany(drawings map[string]generativeart.Engine) {
-    for k := range drawings {
-        drawOne(k)
-    }
-}
-
-func drawOne(art string) string {
-    rand.New(rand.NewSource(time.Now().Unix()))
-    c := generativeart.NewCanva(600, 400)
-    c.SetColorSchema([]color.RGBA{
-        {0xCF, 0x2B, 0x34, 0xFF},
-        {0xF0, 0x8F, 0x46, 0xFF},
-        {0xF0, 0xC1, 0x29, 0xFF},
-        {0x19, 0x6E, 0x94, 0xFF},
-        {0x35, 0x3A, 0x57, 0xFF},
-    })
-    c.SetBackground(common.NavajoWhite)
-    c.FillBackground()
-    c.SetLineWidth(1.0)
-    c.SetLineColor(common.Orange)
-    c.Draw(DRAWINGS[art])
-
-    fileName := fmt.Sprintf("/tmp/%s_%f.png", art, rand.Float64())
-    if err := c.ToPNG(fileName); err != nil {
-        fmt.Printf("Error saving file %s: %v\n", fileName, err)
-    }
-    return fileName
-}
-
