c5c72bce26bf5091912c5f93a3d0d977ea3a62d8
duolok <dusan.lecic5@gmail.com>
2024-08-24 20:59:19 +0200

    add: list selector

--- main.go
+++ main.go
@@ -1,30 +1,44 @@
 package main
 
 import (
-    "github.com/gin-gonic/gin"
-    "tonic/generator"
+	"github.com/gin-gonic/gin"
+	"golang.org/x/exp/maps"
+	"net/http"
+	"tonic/generator"
 )
 
 func router() *gin.Engine {
-    r := gin.Default()
-    userRoute := r.Group("/image")
-    {
-        userRoute.GET("/circles", func(c *gin.Context) {
-            file := generator.DrawOne("circles")
-            c.Header("Content-Type", "image/png")
-            c.File(file)
-        })
-        userRoute.GET("/:generator", func(c *gin.Context) {
-            engine := c.Param("generator")
-            file := generator.DrawOne(engine)
-            c.Header("Content-Type", "image/png")
-            c.File(file)
-        })
-    }
+	r := gin.Default()
+	r.LoadHTMLGlob("templates/*.tmpl")
 
-    return r
+	userRoute := r.Group("/image")
+	{
+		userRoute.GET("/circles", func(c *gin.Context) {
+			file := generator.DrawOne("circles")
+			c.Header("Content-Type", "image/png")
+			c.File(file)
+		})
+		userRoute.GET("/:generator", func(c *gin.Context) {
+			engine := c.Param("generator")
+			file := generator.DrawOne(engine)
+			c.Header("Content-Type", "image/png")
+			c.File(file)
+		})
+
+	}
+
+	listRoute := r.Group("/list")
+	{
+		listRoute.GET("/simple", func(c *gin.Context) {
+			c.HTML(http.StatusOK, "simple.tmpl", gin.H{
+				"keys": maps.Keys(generator.DRAWINGS),
+			})
+		})
+
+		return r
+	}
 }
 
 func main() {
-    router().Run()
+	router().Run()
 }
