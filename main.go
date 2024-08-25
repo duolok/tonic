d9c3a81b3294f9ac7a8a0581db2391bb2ff54968
duolok <dusan.lecic5@gmail.com>
2024-08-25 22:15:10 +0200

    add: add synchornized map for caching

--- main.go
+++ main.go
@@ -1,15 +1,49 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
-	"golang.org/x/exp/maps"
+	"context"
+	"encoding/json"
+	"fmt"
+	"math/rand"
 	"net/http"
+	"strconv"
+	"sync"
+	"time"
 	"tonic/generator"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-queue/queue"
+	"github.com/golang-queue/queue/core"
+	"golang.org/x/exp/maps"
 )
+
+type JobData struct {
+	Id        string
+	Generator string
+}
+
+var sm sync.Map
+
+func (j *JobData) Bytes() []byte {
+	b, _ := json.Marshal(j)
+	return b
+}
 
 func router() *gin.Engine {
 	r := gin.Default()
 	r.LoadHTMLGlob("templates/*.tmpl")
+
+	rand.New(rand.NewSource(time.Now().Unix()))
+	q := queue.NewPool(30, queue.WithFn(func(ctx context.Context, m core.QueuedMessage) error {
+		j, _ := m.(*JobData)
+		json.Unmarshal(m.Bytes(), &j)
+
+		path := generator.DrawOne(j.Generator)
+		sm.Store(j.Id, path)
+		fmt.Printf("Stored %s:%s [%s]", j.Id, j.Generator, path)
+
+		return nil
+	}))
 
 	userRoute := r.Group("/image")
 	{
@@ -34,9 +68,39 @@ func router() *gin.Engine {
 				"keys": maps.Keys(generator.DRAWINGS),
 			})
 		})
-
-		return r
 	}
+
+	newRoute := r.Group("/new")
+	{
+		newRoute.GET("/load/:id", func(c *gin.Context) {
+			id := c.Param("id")
+			path, ok := sm.Load(id)
+
+			if ok {
+				fmt.Printf("Found %s for id: %s\n", path, id)
+				c.Header("Content-Type", "image/png")
+				c.File(fmt.Sprintf("%s", path.(string)))
+			} else {
+				fmt.Printf("Path not found for id: %s\n", id)
+				c.Header("Content-Type", "image/jpg")
+				c.Header("Cache-Control", "no-cache")
+				c.File("static/loading.jpg")
+			}
+		})
+		newRoute.GET("/:generator", func(c *gin.Context) {
+			generator := c.Param("generator")
+			newJob := JobData{
+				Id:        strconv.Itoa(rand.Int()),
+				Generator: generator,
+			}
+			q.Queue(&newJob)
+			res := map[string]string{"id": newJob.Id, "url": "http://" + c.Request.Host + "/new/load/" + newJob.Id}
+			c.JSON(200, res)
+		})
+	}
+
+	return r
+
 }
 
 func main() {
