ad6002a7018428c87b9a9126d125d9d8dece6520
duolok <dusan.lecic5@gmail.com>
2024-09-04 16:44:30 +0200

    add: edit color schemes

--- generator/drawing.go
+++ generator/drawing.go
@@ -19,22 +19,29 @@ var DRAWINGS = map[string]generativeart.Engine{
     "random":    arts.NewRandomShape(150),
     "silksky":   arts.NewSilkSky(15, 5),
     "circles":   arts.NewColorCircle2(30),
+    "contour":   arts.NewContourLine(5000),
+    "noise":     arts.NewNoiseLine(1000),
+    "circle":    arts.NewCircleLoop(100),
+    "dot":       arts.NewDotLine(100, 20, 50, false),
+    "colorcanva":arts.NewColorCanve(10),
 }
 
 func DrawOne(art string) string {
     rand.New(rand.NewSource(time.Now().Unix()))
-    c := generativeart.NewCanva(600, 400)
+    c := generativeart.NewCanva(1920, 1080)
     c.SetColorSchema([]color.RGBA{
-        {0xCF, 0x2B, 0x34, 0xFF},
-        {0xF0, 0x8F, 0x46, 0xFF},
-        {0xF0, 0xC1, 0x29, 0xFF},
-        {0x19, 0x6E, 0x94, 0xFF},
-        {0x35, 0x3A, 0x57, 0xFF},
+        {0x58, 0x18, 0x45, 0xFF},
+        {0x90, 0x0C, 0x3F, 0xFF},
+        {0xC7, 0x00, 0x39, 0xFF},
+        {0xFF, 0x57, 0x33, 0xFF},
+        {0xFF, 0xC3, 0x0F, 0xFF},
     })
-    c.SetBackground(common.NavajoWhite)
+
+    c.SetBackground(color.RGBA{R: 30, G: 30, B: 30, A: 225})
+    c.SetIterations(250)
     c.FillBackground()
     c.SetLineWidth(1.0)
-    c.SetLineColor(common.Orange)
+    c.SetLineColor(common.Tomato)
     c.Draw(DRAWINGS[art])
 
     fileName := fmt.Sprintf("/tmp/%s_%f.png", art, rand.Float64())
